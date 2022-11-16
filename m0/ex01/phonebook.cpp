81da86ae8783e8533df39fb944dbcc417fdd9bb6
Hiroki Yamamoto <hymt73@gmail.com>
2022-11-16 19:00:38 +0100

    working

--- m0/ex01/phonebook.cpp
+++ m0/ex01/phonebook.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -6,23 +7,137 @@ class PhoneBook
 {
 	private:
 		string firstName;
-		string lasttName;
+		string lastName;
 		string nickname;
 		int phoneNumber;
 		string darkestSecret;
 
+	public:
+		void setFirstName(string firstName)
+		{
+			this->firstName = firstName;
+		}
+		void setLastName(string lastName)
+		{
+			this->lastName = lastName;
+		}
+		void setNickname(string nickname)
+		{
+			this->nickname = nickname;
+		}
+		void setPhoneNumber(int phoneNumber)
+		{
+			this->phoneNumber = phoneNumber;
+		}
+		void setDarkestSecret(string darkestSecret)
+		{
+			this->darkestSecret = darkestSecret;
+		}
 
+		string getFirstName()
+		{
+			return firstName;
+		}
+		string getLastName()
+		{
+			return lastName;
+		}
+		string getNickname()
+		{
+			return nickname;
+		}
+		int getPhoneNumber()
+		{
+			return phoneNumber;
+		}
+		string getDarkestSecret()
+		{
+			return darkestSecret;
+		}
 
 };
 
+void add(int i, PhoneBook p[])
+{
+	string i1;
+	string i2;
+	string i3;
+	int i4;
+	string i5;
+
+	cout << "Type your first name: ";
+	cin >> i1;
+	cout << "Type your last name: ";
+	cin >> i2;
+	cout << "Type your nickname: ";
+	cin >> i3;
+	cout << "Type your phone number: "; //TODO: retry when it is not a number
+	cin >> i4;
+	cout << "Type your darkest secret: ";
+	cin >> i5;
+	p[i % 8].setFirstName(i1);
+	p[i % 8].setLastName(i2);
+	p[i % 8].setNickname(i3);
+	p[i % 8].setPhoneNumber(i4);
+	p[i % 8].setDarkestSecret(i5);
+}
+
+void search(int i, PhoneBook p[])
+{
+	int k = 0;
+	string firstName;
+	string lastName;
+	string nickname;
+	while (k < 8)
+	{
+		if (p[k].getFirstName() == "")
+			break;
+
+		cout << setw(10);
+		cout << k << "|";
+
+		cout << setw(10);
+		firstName = p[k].getFirstName();
+		if (firstName.length() > 10)
+			cout << firstName.substr(0, 9) << ".";
+		else
+ 			cout << firstName;
+		cout << "|";
+
+		cout << setw(10);
+		lastName = p[k].getLastName();
+		if (lastName.length() > 10)
+			cout << lastName.substr(0, 9) << ".";
+		else
+			cout << lastName;
+		cout << "|";
+
+		cout << setw(10);
+		nickname = p[k].getNickname();
+		if (nickname.length() > 10)
+			cout << nickname.substr(0, 9) << "." << "\n";
+		else
+			cout << nickname << "\n";
+		k++;
+	}
+}
+
 int main(int argc, char** argv)
 {
-	string input;
+	string input = "";
 	PhoneBook p[8];
+	int i = 0;
 
-	while (input != "EXIT")
+	while (1)
 	{
 		cin >> input;
+		if (input.compare("EXIT") == 0)
+			break;
+		if (input.compare("ADD") == 0)
+			add(i++, p);
+		if (input.compare("SEARCH") == 0)
+			search(i, p);
+		
 	}
 	return 0;
 }
