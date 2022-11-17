4a6f154688b705835ef1494e144aef0b7f125d85
Hiroki Yamamoto <hymt73@gmail.com>
2022-11-17 19:30:31 +0100

    fixed warning

--- m0/ex02/Account.cpp
+++ m0/ex02/Account.cpp
@@ -1,4 +1,3 @@
-#include <chrono>
 #include <iomanip>
 #include <iostream>
 #include "Account.hpp"
@@ -24,9 +23,6 @@ int	Account::getNbWithdrawals( void ) {
 	return _totalNbWithdrawals;
 }
 
-/*
-[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
-*/
 void	Account::displayAccountsInfos( void ) {
 	_displayTimestamp();
 	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
@@ -46,16 +42,6 @@ Account::~Account( void ) {
 	std::cout << "index:" << this->_accountIndex << ";amount:" << checkAmount() << ";closed" << std::endl;
 }
 
-/*
-[19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
-[19920104_091532] index:1;p_amount:54;deposit:765;amount:819;nb_deposits:1
-[19920104_091532] index:2;p_amount:957;deposit:564;amount:1521;nb_deposits:1
-[19920104_091532] index:3;p_amount:432;deposit:2;amount:434;nb_deposits:1
-[19920104_091532] index:4;p_amount:1234;deposit:87;amount:1321;nb_deposits:1
-[19920104_091532] index:5;p_amount:0;deposit:23;amount:23;nb_deposits:1
-[19920104_091532] index:6;p_amount:754;deposit:9;amount:763;nb_deposits:1
-[19920104_091532] index:7;p_amount:16576;deposit:20;amount:16596;nb_deposits:1
-*/
 void	Account::makeDeposit( int deposit ) {
 	int currentAmount = checkAmount();
 	this->_amount = currentAmount + deposit;
@@ -66,9 +52,6 @@ void	Account::makeDeposit( int deposit ) {
 	std::cout << "index:" << this->_accountIndex << ";p_amount:" << currentAmount << ";deposit:" << deposit << ";amount:" << checkAmount() << ";nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
-
-// [19920104_091532] index:5;p_amount:23;withdrawal:refused
-// [19920104_091532] index:6;p_amount:763;withdrawal:657;amount:106;nb_withdrawals:1
 bool	Account::makeWithdrawal( int withdrawal ) {
 	int currentAmount = checkAmount();
 	if (withdrawal <= currentAmount) {
@@ -78,10 +61,11 @@ bool	Account::makeWithdrawal( int withdrawal ) {
 		Account::_totalNbWithdrawals++;
 		_displayTimestamp();
 		std::cout << "index:" << this->_accountIndex << ";p_amount:" << currentAmount << ";withdrawal:" << withdrawal << ";amount:" << checkAmount() << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
-	} else {
-		_displayTimestamp();
-		std::cout << "index:" << this->_accountIndex << ";p_amount:" << currentAmount << ";withdrawal:refused" << std::endl;
+		return true;
 	}
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << currentAmount << ";withdrawal:refused" << std::endl;
+	return false;
 }
 
 int		Account::checkAmount( void ) const {
@@ -94,7 +78,6 @@ void	Account::displayStatus( void ) const {
 }
 
 void	Account::_displayTimestamp( void ) {
-	const auto now = std::chrono::system_clock::now();
-	const auto now_t = std::chrono::system_clock::to_time_t(now);
+	time_t now_t = time(0);
 	std::cout << "[" << std::put_time(std::localtime(&now_t), "%Y%m%d_%H%M%S") << "] ";
 }
