d752ad4dcaaee9f91f359a6b327bf8ba3be6c1b2
Hiroki Yamamoto <hymt73@gmail.com>
2023-02-01 20:06:46 +0100

    fixed

--- m6/ex01/main.cpp
+++ m6/ex01/main.cpp
@@ -13,15 +13,16 @@ Data* deserialize(uintptr_t raw) {
 }
 
 int main() {
-	Data *data;
+	Data *data_1;
+	Data *data_2;
 	uintptr_t raw;
 
-	data = new Data;
-	data->n = 42;
-	std::cout << "original: " << data->n << std::endl;
-	raw = serialize(data);
-	delete data;
-	data = deserialize(raw);
-	std::cout << "after: " << data->n << std::endl;
+	data_1 = new Data;
+	data_1->n = 42;
+	std::cout << "data_1: " << data_1->n << std::endl;
+	raw = serialize(data_1);
+	delete data_1;
+	data_2 = deserialize(raw);
+	std::cout << "data_2: " << data_2->n << std::endl;
 	return 0;
 }
