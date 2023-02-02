52f5c09594c8d4ce716528ebf5239019895d4729
Hiroki Yamamoto <hymt73@gmail.com>
2023-02-02 15:02:50 +0100

    working on

---
+++ m6/ex02/main.cpp
@@ -0,0 +1,21 @@
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+
+Base* generate() {
+	
+}
+
+void identify(Base* p) {
+	
+}
+
+void identify(Base& p) {
+
+}
+
+int main() {
+
+	return 0;
+}
